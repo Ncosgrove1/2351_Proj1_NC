af67506abb0f719f0dc8b1107def77eb4341abd5
Nathan Cosgrove <ncosgrove272@gmail.com>
2023-09-11 01:17:04 -0500

    Added Variables and simple mathematical operations

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("How long have you been coding, professor?") ;
+var a = 1 ;
+
+const b = 2 ; 
+
+let c = 3 ; 
+
+let x = (25 +30) * c ;
+
+let y = b * c ;
+
+
+
+console.log("The variables are listed below", '\n', "var a = 1 ", '\n' , "const b = 2", '\n', "let c = 3", '\n', "let x = (25+30) * c", '\n' 
+    ,"Therefore x =",x,'\n', "And y =", y) ;
